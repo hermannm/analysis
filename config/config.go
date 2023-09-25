176106cd1919f57c883cf4a7fdefa30863fb18ac
hermannm <hermann.morkrid@gmail.com>
2023-09-25 15:12:30 +0200

    add DATABASE env to config

--- config/config.go
+++ config/config.go
@@ -1,15 +1,22 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/caarlos0/env/v9"
 	"github.com/joho/godotenv"
 	"hermannm.dev/wrap"
 )
 
 type Config struct {
-	IsProduction bool `env:"PRODUCTION"`
+	BaseConfig
+	ClickHouse ClickHouse
+}
+
+type BaseConfig struct {
+	IsProduction bool        `env:"PRODUCTION"`
+	DB           SupportedDB `env:"DATABASE"`
 	API          API
-	ClickHouse   ClickHouse
 }
 
 type API struct {
@@ -25,14 +32,33 @@ type ClickHouse struct {
 	DropTableOnStartup string `env:"DEBUG_DROP_TABLE_ON_STARTUP" envDefault:""`
 }
 
+type SupportedDB string
+
+const (
+	DBClickHouse SupportedDB = "clickhouse"
+)
+
 func ReadFromEnv() (Config, error) {
 	if err := godotenv.Load(); err != nil {
 		return Config{}, wrap.Error(err, "failed to load .env file")
 	}
 
+	parseOptions := env.Options{RequiredIfNoDef: true}
+
 	var config Config
-	if err := env.ParseWithOptions(&config, env.Options{RequiredIfNoDef: true}); err != nil {
+
+	if err := env.ParseWithOptions(&config.BaseConfig, parseOptions); err != nil {
 		return Config{}, err
+	}
+
+	switch config.DB {
+	case DBClickHouse:
+		if err := env.ParseWithOptions(&config.ClickHouse, parseOptions); err != nil {
+			return Config{}, err
+		}
+	default:
+		err := fmt.Errorf("must be one of: '%s'", DBClickHouse)
+		return Config{}, wrap.Errorf(err, "unsupported value '%s' for DATABASE in env", config.DB)
 	}
 
 	return config, nil
--- main.go
+++ main.go
@@ -5,28 +5,33 @@ import (
 
 	"hermannm.dev/analysis/api"
 	"hermannm.dev/analysis/config"
+	"hermannm.dev/analysis/db"
 	"hermannm.dev/analysis/db/clickhouse"
 	"hermannm.dev/analysis/log"
 )
 
 func main() {
 	log.Info("Loading environment variables...")
-	config, err := config.ReadFromEnv()
+	conf, err := config.ReadFromEnv()
 	if err != nil {
 		log.Error(err, "failed to read config from env")
 		os.Exit(1)
 	}
 
-	log.Info("Connecting to ClickHouse...")
-	db, err := clickhouse.NewClickHouseDB(config)
-	if err != nil {
-		log.Error(err, "failed to initialize database")
-		os.Exit(1)
+	var db db.AnalysisDB
+	switch conf.DB {
+	case config.DBClickHouse:
+		log.Info("Connecting to ClickHouse...")
+		db, err = clickhouse.NewClickHouseDB(conf)
+		if err != nil {
+			log.Error(err, "failed to initialize database")
+			os.Exit(1)
+		}
 	}
 
-	analysisAPI := api.NewAnalysisAPI(db, config)
+	analysisAPI := api.NewAnalysisAPI(db, conf)
 
-	log.Infof("Listening on port %s...", config.API.Port)
+	log.Infof("Listening on port %s...", conf.API.Port)
 	if err := analysisAPI.ListenAndServe(); err != nil {
 		log.Error(err, "server stopped")
 		os.Exit(1)
