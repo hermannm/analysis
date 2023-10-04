d4dd65a46a878b6d12f3bba1cae519fc8001a149
hermannm <hermann.morkrid@gmail.com>
2023-10-04 12:46:47 +0200

    move dropTable from utils.go -> clickhouse.go

--- db/clickhouse/clickhouse.go
+++ db/clickhouse/clickhouse.go
@@ -6,6 +6,7 @@ import (
 
 	clickhousego "github.com/ClickHouse/clickhouse-go/v2"
 	clickhousedriver "github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	clickhouseproto "github.com/ClickHouse/clickhouse-go/v2/lib/proto"
 	"hermannm.dev/analysis/config"
 	"hermannm.dev/analysis/log"
 	"hermannm.dev/wrap"
@@ -56,4 +57,31 @@ func NewClickHouseDB(config config.Config) (ClickHouseDB, error) {
 	}
 
 	return clickhouse, nil
+}
+
+func (clickhouse ClickHouseDB) dropTable(
+	ctx context.Context,
+	table string,
+) (tableAlreadyDropped bool, err error) {
+	if err := ValidateIdentifier(table); err != nil {
+		return false, wrap.Error(err, "invalid table name")
+	}
+
+	var builder QueryBuilder
+	builder.WriteString("DROP TABLE ")
+	builder.WriteIdentifier(table)
+
+	// See https://github.com/ClickHouse/ClickHouse/blob/bd387f6d2c30f67f2822244c0648f2169adab4d3/src/Common/ErrorCodes.cpp#L66
+	const clickhouseUnknownTableErrorCode = 60
+
+	if err := clickhouse.conn.Exec(ctx, builder.String()); err != nil {
+		clickHouseErr, isClickHouseErr := err.(*clickhouseproto.Exception)
+		if isClickHouseErr && clickHouseErr.Code == clickhouseUnknownTableErrorCode {
+			return true, nil
+		}
+
+		return false, wrap.Error(err, "drop table query failed")
+	}
+
+	return false, nil
 }
