2ca10fc5c7deaf0e4fe5360a950df6b4b60c2d19
hermannm <hermann.morkrid@gmail.com>
2023-11-02 11:50:33 +0100

    implement elasticsearch request building for analysis queries

--- db/elasticsearch/analysis.go
+++ db/elasticsearch/analysis.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 
 	"github.com/elastic/go-elasticsearch/v8/typedapi/core/search"
-	elastictypes "github.com/elastic/go-elasticsearch/v8/typedapi/types"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
 	"hermannm.dev/analysis/db"
 	"hermannm.dev/wrap"
 )
@@ -33,13 +33,130 @@ func (elastic ElasticsearchDB) RunAnalysisQuery(
 	return analysisResult, nil
 }
 
+const (
+	columnSplitName      = "column_split"
+	rowSplitName         = "row_split"
+	valueAggregationName = "value_aggregation"
+)
+
 func (elastic ElasticsearchDB) buildAnalysisQueryRequest(
 	analysis db.AnalysisQuery,
 ) (*search.Search, error) {
-	aggregations := make(map[string]elastictypes.Aggregations, 3)
-	query := elastictypes.NewQuery()
+	query := types.NewQuery()
 
-	return elastic.client.Search().Query(query).Aggregations(aggregations), nil
+	rowSplit, err := createSplit(analysis.RowSplit)
+	if err != nil {
+		return nil, wrap.Error(err, "failed to create row split")
+	}
+
+	columnSplit, err := createSplit(analysis.ColumnSplit)
+	if err != nil {
+		return nil, wrap.Error(err, "failed to create column split")
+	}
+
+	valueAggregation, err := createValueAggregation(analysis.ValueAggregation)
+	if err != nil {
+		return nil, wrap.Error(err, "failed to create value aggregation")
+	}
+
+	columnSplit.Aggregations = map[string]types.Aggregations{
+		valueAggregationName: valueAggregation,
+	}
+	rowSplit.Aggregations = map[string]types.Aggregations{
+		columnSplitName: columnSplit,
+	}
+	aggregations := map[string]types.Aggregations{
+		rowSplitName: rowSplit,
+	}
+
+	// Size 0, since we only want aggregation results
+	// https://www.elastic.co/guide/en/elasticsearch/reference/8.10/search-aggregations.html#return-only-agg-results
+	return elastic.client.Search().Size(0).Query(query).Aggregations(aggregations), nil
+}
+
+func createSplit(split db.Split) (types.Aggregations, error) {
+	field := split.BaseColumnName
+
+	switch split.BaseColumnDataType {
+	case db.DataTypeInt, db.DataTypeFloat:
+		isInt := split.BaseColumnDataType == db.DataTypeInt
+
+		if (isInt && split.IntegerInterval != 0) || (!isInt && split.FloatInterval != 0) {
+			var interval types.Float64
+			if isInt {
+				interval = types.Float64(split.IntegerInterval)
+			} else {
+				interval = types.Float64(split.FloatInterval)
+			}
+
+			sortOrder, err := sortOrderToElasticBucket(split.SortOrder)
+			if err != nil {
+				return types.Aggregations{}, err
+			}
+
+			// Histogram is a bucket aggregation for number ranges
+			// The intervals are placed in buckets by value based on this formula:
+			//   Math.floor((value - offset) / interval) * interval + offset
+			// Since we don't give an offset, this is the same formula as the one we use for
+			// ClickHouse (see clickhouse/query_builder.go -> QueryBuilder.WriteSplit)
+			// https://www.elastic.co/guide/en/elasticsearch/reference/8.10/search-aggregations-bucket-histogram-aggregation.html
+			return types.Aggregations{Histogram: &types.HistogramAggregation{
+				Field:    &field,
+				Interval: &interval,
+				Order:    sortOrder,
+			}}, nil
+		}
+	case db.DataTypeTimestamp:
+		if split.DateInterval != nil {
+			dateInterval, ok := dateIntervalToElastic(*split.DateInterval)
+			if !ok {
+				return types.Aggregations{}, errors.New("invalid date interval")
+			}
+
+			sortOrder, err := sortOrderToElasticBucket(split.SortOrder)
+			if err != nil {
+				return types.Aggregations{}, err
+			}
+
+			// DateHistogram is a bucket aggregation for date ranges
+			// https://www.elastic.co/guide/en/elasticsearch/reference/8.10/search-aggregations-bucket-datehistogram-aggregation.html
+			return types.Aggregations{DateHistogram: &types.DateHistogramAggregation{
+				Field:            &field,
+				CalendarInterval: &dateInterval,
+				Order:            sortOrder,
+			}}, nil
+		}
+	}
+
+	// If we get here, no interval was specified, so we want to use the 'Terms' bucket aggregation
+	// to group by unique values
+	// https://www.elastic.co/guide/en/elasticsearch/reference/8.10/search-aggregations-bucket-terms-aggregation.html
+	terms := types.NewTermsAggregation()
+	terms.Field = &field
+	return types.Aggregations{Terms: terms}, nil
+}
+
+func createValueAggregation(valueAggregation db.ValueAggregation) (types.Aggregations, error) {
+	if err := valueAggregation.BaseColumnDataType.IsValidForAggregation(); err != nil {
+		return types.Aggregations{}, err
+	}
+
+	field := valueAggregation.BaseColumnName
+
+	switch valueAggregation.Aggregation {
+	case db.AggregationSum:
+		return types.Aggregations{Sum: &types.SumAggregation{Field: &field}}, nil
+	case db.AggregationAverage:
+		return types.Aggregations{Avg: &types.AverageAggregation{Field: &field}}, nil
+	case db.AggregationMin:
+		return types.Aggregations{Min: &types.MinAggregation{Field: &field}}, nil
+	case db.AggregationMax:
+		return types.Aggregations{Max: &types.MaxAggregation{Field: &field}}, nil
+	case db.AggregationCount:
+		return types.Aggregations{Cardinality: &types.CardinalityAggregation{Field: &field}}, nil
+	default:
+		return types.Aggregations{}, errors.New("invalid aggregation type")
+	}
 }
 
 func parseAnalysisQueryResponse(
--- db/elasticsearch/conversion.go
+++ db/elasticsearch/conversion.go
@@ -1,9 +1,12 @@
 package elasticsearch
 
 import (
+	"errors"
 	"fmt"
 
 	elastictypes "github.com/elastic/go-elasticsearch/v8/typedapi/types"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types/enums/calendarinterval"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types/enums/sortorder"
 	"hermannm.dev/analysis/db"
 	"hermannm.dev/wrap"
 )
@@ -26,6 +29,47 @@ func schemaToElasticMappings(schema db.TableSchema) (*elastictypes.TypeMapping, 
 	}
 
 	return mappings, nil
+}
+
+func sortOrderToElastic(sortOrder db.SortOrder) (elasticSortOrder sortorder.SortOrder, ok bool) {
+	switch sortOrder {
+	case db.SortOrderAscending:
+		return sortorder.Asc, true
+	case db.SortOrderDescending:
+		return sortorder.Desc, true
+	default:
+		return sortorder.SortOrder{}, false
+	}
+}
+
+func sortOrderToElasticBucket(sortOrder db.SortOrder) (map[string]sortorder.SortOrder, error) {
+	elasticSortOrder, ok := sortOrderToElastic(sortOrder)
+	if !ok {
+		return nil, errors.New("invalid sort order")
+	}
+
+	// Bucket aggregation results have a "key" field with the bucket value
+	// https://www.elastic.co/guide/en/elasticsearch/reference/8.10/search-aggregations-bucket-histogram-aggregation.html#_order_2
+	return map[string]sortorder.SortOrder{"key": elasticSortOrder}, nil
+}
+
+func dateIntervalToElastic(
+	dateInterval db.DateInterval,
+) (elasticDateInterval calendarinterval.CalendarInterval, ok bool) {
+	switch dateInterval {
+	case db.DateIntervalYear:
+		return calendarinterval.Year, true
+	case db.DateIntervalQuarter:
+		return calendarinterval.Quarter, true
+	case db.DateIntervalMonth:
+		return calendarinterval.Month, true
+	case db.DateIntervalWeek:
+		return calendarinterval.Week, true
+	case db.DateIntervalDay:
+		return calendarinterval.Day, true
+	default:
+		return calendarinterval.CalendarInterval{}, false
+	}
 }
 
 func dataTypeToElasticProperty(dataType db.DataType) (elastictypes.Property, error) {
