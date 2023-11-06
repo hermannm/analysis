dd7783a2f792d041979c35f2a20eab8aa919e874
hermannm <hermann.morkrid@gmail.com>
2023-11-06 14:13:15 +0100

    fix elasticsearch data ingestion

--- db/elasticsearch/ingestion.go
+++ db/elasticsearch/ingestion.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 
 	"github.com/elastic/go-elasticsearch/v8/esutil"
 	"github.com/google/uuid"
@@ -36,15 +37,18 @@ func (elastic ElasticsearchDB) UpdateTableData(
 	schema db.TableSchema,
 	data db.DataSource,
 ) error {
+	ctx, cancel := context.WithCancelCause(ctx)
+
 	bulk, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
 		Client: elastic.untypedClient,
 		Index:  schema.TableName,
+		OnError: func(ctx context.Context, err error) {
+			cancel(formatElasticError(extractElasticError(err)))
+		},
 	})
 	if err != nil {
 		return wrap.Error(err, "failed to prepare bulk data insert")
 	}
-
-	ctx, cancel := context.WithCancelCause(ctx)
 
 	for {
 		row, rowNumber, done, err := data.ReadRow()
@@ -52,53 +56,62 @@ func (elastic ElasticsearchDB) UpdateTableData(
 			break
 		}
 		if err != nil {
-			return wrap.Error(err, "failed to read row")
+			cancel(wrap.Error(err, "failed to read row"))
+			break
 		}
 
 		id, err := uuid.NewUUID()
 		if err != nil {
-			return wrap.Errorf(err, "failed to generate unique ID for row %d", rowNumber)
+			cancel(wrap.Errorf(err, "failed to generate unique ID for row %d", rowNumber))
+			break
 		}
 		idString := id.String()
 
 		rowMap, err := schema.ConvertRowToMap(row)
 		if err != nil {
-			return wrap.Errorf(
+			cancel(wrap.Errorf(
 				err,
 				"failed to convert row %d to data types expected by table schema",
 				rowNumber,
-			)
+			))
+			break
 		}
 
 		rowJSON, err := json.Marshal(rowMap)
 		if err != nil {
-			return wrap.Errorf(
+			cancel(wrap.Errorf(
 				err,
 				"failed to encode row %d to JSON for sending to Elasticsearch",
 				rowNumber,
-			)
+			))
+			break
 		}
 
 		if err := bulk.Add(ctx, esutil.BulkIndexerItem{
+			// https://www.elastic.co/guide/en/elasticsearch/reference/8.10/docs-bulk.html#docs-bulk-api-desc
+			Action:     "create",
 			DocumentID: idString,
 			Body:       bytes.NewReader(rowJSON),
 			OnFailure: func(ctx context.Context, item esutil.BulkIndexerItem, response esutil.BulkIndexerResponseItem, err error) {
 				cancel(wrap.Errorf(err, "failed to insert row %d", rowNumber))
 			},
 		}); err != nil {
-			return wrap.Errorf(err, "failed to add row %d to bulk insert", rowNumber)
+			if err != context.Canceled {
+				cancel(wrap.Errorf(err, "failed to add row %d to bulk insert", rowNumber))
+			}
+			break
 		}
 	}
 
 	if err := bulk.Close(ctx); err != nil {
-		return wrap.Error(err, "failed to finish bulk insert")
-	}
-
-	if err := ctx.Err(); err != nil {
-		if cause := context.Cause(ctx); cause != nil {
-			return cause
+		cause := context.Cause(ctx)
+		if cause == nil {
+			return wrap.Error(err, "failed to finish Elasticsearch bulk insert")
+		} else if cause == context.Canceled {
+			//lint:ignore ST1005 Names in errors should still be capitalized
+			return errors.New("Elasticsearch bulk insert was canceled before completion")
 		} else {
-			return wrap.Error(err, "bulk insert was canceled with error")
+			return wrap.Error(cause, "Elasticsearch bulk insert was canceled by error")
 		}
 	}
 
